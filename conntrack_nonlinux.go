c9631b72844b8e9ca470c7ac9088958b966b8a23
Lehner Florian <dev@der-flo.net>
2018-07-06 09:01:37 +0200

    Improve handling of non linux OS
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- conntrack_nonlinux.go
+++ conntrack_nonlinux.go
@@ -4,7 +4,35 @@ package conntrack
 
 import "errors"
 
+// ErrNotLinux is returned when using this package on a non linux OS
+var ErrNotLinux = errors.New("package requires the netfilter subsystem of the linux kernel")
+
+// Nfct represents a conntrack handler
+type Nfct struct {
+}
+
+// Conn contains all the information of a connection
+type Conn map[ConnAttrType][]byte
+
+// CtFamily specifies the network family
+type CtFamily uint8
+
 // Open returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
 func Open() (*Nfct, error) {
-	return nil, errors.New("conntrack requires the netfilter subsystem of the linux kernel")
+	return nil, ErrNotLinux
+}
+
+// Close returns an error, as this package highly depends on the netfilter subsystem of the linux kernel
+func (nfct *Nfct) Close() error {
+	return ErrNotLinux
+}
+
+// Dump returns an error, as this package highly depends on the netfilter subsystem of the linux kernel
+func (nfct *Nfct) Dump(f CtFamily) ([]Conn, error) {
+	return nil, ErrNotLinux
+}
+
+// Flush returns an error, as this package highly depends on the netfilter subsystem of the linux kernel
+func (nfct *Nfct) Flush(f CtFamily) error {
+	return ErrNotLinux
 }
