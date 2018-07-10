29199bf65615959987dbc98166bca2b887ef1e1d
Lehner Florian <dev@der-flo.net>
2018-07-10 19:34:01 +0200

    Improve handling of nested attributes
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- nest.go
+++ nest.go
@@ -18,14 +18,14 @@ type attrCheckStruct struct {
 }
 
 var attrCheck = map[ConnAttrType]attrCheckStruct{
-	AttrOrigIPv4Src:             {len: 4, ct: ctaUnspec},
-	AttrOrigIPv4Dst:             {len: 4, ct: ctaUnspec},
-	AttrReplIPv4Src:             {len: 4, ct: ctaUnspec},
-	AttrReplIPv4Dst:             {len: 4, ct: ctaUnspec},
-	AttrOrigIPv6Src:             {len: 16, ct: ctaUnspec},
-	AttrOrigIPv6Dst:             {len: 16, ct: ctaUnspec},
-	AttrReplIPv6Src:             {len: 16, ct: ctaUnspec},
-	AttrReplIPv6Dst:             {len: 16, ct: ctaUnspec},
+	AttrOrigIPv4Src:             {len: 4, ct: ctaIPv4Src},
+	AttrOrigIPv4Dst:             {len: 4, ct: ctaIPv4Dst},
+	AttrReplIPv4Src:             {len: 4, ct: ctaIPv4Src},
+	AttrReplIPv4Dst:             {len: 4, ct: ctaIPv4Dst},
+	AttrOrigIPv6Src:             {len: 16, ct: ctaIPv6Src},
+	AttrOrigIPv6Dst:             {len: 16, ct: ctaIPv6Dst},
+	AttrReplIPv6Src:             {len: 16, ct: ctaIPv6Src},
+	AttrReplIPv6Dst:             {len: 16, ct: ctaIPv6Dst},
 	AttrOrigPortSrc:             {len: 2, ct: ctaUnspec},
 	AttrOrigPortDst:             {len: 2, ct: ctaUnspec},
 	AttrReplPortSrc:             {len: 2, ct: ctaUnspec},
@@ -35,8 +35,8 @@ var attrCheck = map[ConnAttrType]attrCheckStruct{
 	AttrIcmpID:                  {len: 2, ct: ctaUnspec},
 	AttrOrigL3Proto:             {len: 1, ct: ctaUnspec},
 	AttrReplL3Proto:             {len: 1, ct: ctaUnspec},
-	AttrOrigL4Proto:             {len: 1, ct: ctaUnspec},
-	AttrReplL4Proto:             {len: 1, ct: ctaUnspec},
+	AttrOrigL4Proto:             {len: 1, ct: ctaProtoNum},
+	AttrReplL4Proto:             {len: 1, ct: ctaProtoNum},
 	AttrTCPState:                {len: 1, ct: ctaUnspec},
 	AttrSNatIPv4:                {len: 4, ct: ctaUnspec},
 	AttrDNatIPv4:                {len: 4, ct: ctaUnspec},
@@ -92,8 +92,46 @@ var attrCheck = map[ConnAttrType]attrCheckStruct{
 	AttrDNatIPv6:                {len: 16, ct: ctaUnspec},
 }
 
+func nestSubTuple(tupleType uint16, sub []netlink.Attribute) ([]byte, error) {
+	attr, err := netlink.MarshalAttributes(sub)
+	if err != nil {
+		return nil, err
+	}
+	var tuple netlink.Attribute
+	tuple.Type = tupleType | nlafNested
+	tuple.Length = uint16(len(attr) + 4)
+	tuple.Data = attr
+
+	return tuple.MarshalBinary()
+}
+
+func nestDirTuple(dir int, ipTuple, protoTuple []netlink.Attribute) ([]byte, error) {
+	ipSub, err := nestSubTuple(1, ipTuple)
+	if err != nil {
+		return nil, err
+	}
+	protoSub, err := nestSubTuple(2, protoTuple)
+	if err != nil {
+		return nil, err
+	}
+	var tuple netlink.Attribute
+	if dir == 1 {
+		tuple.Type = ctaTupleOrig | nlafNested
+	} else {
+		tuple.Type = ctaTupleReply | nlafNested
+	}
+	tuple.Length = uint16(len(ipSub) + len(protoSub) + 4)
+	tuple.Data = ipSub
+	tuple.Data = append(tuple.Data, protoSub...)
+
+	return tuple.MarshalBinary()
+}
+
 func nestAttributes(filters []ConnAttr) ([]byte, error) {
+	var attributes []byte
 	var attrs []netlink.Attribute
+	var tupleOrig, tupleRepl []netlink.Attribute
+	var protoOrig, protoRepl []netlink.Attribute
 
 	for _, filter := range filters {
 		if _, ok := attrCheck[filter.Type]; !ok {
@@ -105,8 +143,44 @@ func nestAttributes(filters []ConnAttr) ([]byte, error) {
 		if len(filter.Data) != attrCheck[filter.Type].len {
 			return nil, ErrAttrLength
 		}
-		attrs = append(attrs, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+		if filter.Type == AttrOrigIPv4Src ||
+			filter.Type == AttrOrigIPv4Dst ||
+			filter.Type == AttrOrigIPv6Src ||
+			filter.Type == AttrOrigIPv6Dst {
+			tupleOrig = append(tupleOrig, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+		} else if filter.Type == AttrReplIPv4Src ||
+			filter.Type == AttrReplIPv4Dst ||
+			filter.Type == AttrReplIPv6Src ||
+			filter.Type == AttrReplIPv6Dst {
+			tupleRepl = append(tupleRepl, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+		} else if filter.Type == AttrOrigL4Proto {
+			protoOrig = append(protoOrig, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+		} else if filter.Type == AttrReplL4Proto {
+			protoRepl = append(protoRepl, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+		} else {
+			attrs = append(attrs, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+		}
 	}
 
-	return netlink.MarshalAttributes(attrs)
+	if len(tupleOrig) != 0 {
+		data, err := nestDirTuple(1, tupleOrig, protoOrig)
+		if err != nil {
+			return nil, err
+		}
+		attributes = append(attributes, data...)
+	}
+	if len(tupleRepl) != 0 {
+		data, err := nestDirTuple(0, tupleRepl, protoRepl)
+		if err != nil {
+			return nil, err
+		}
+		attributes = append(attributes, data...)
+	}
+
+	regular, err := netlink.MarshalAttributes(attrs)
+	if err != nil {
+		return nil, err
+	}
+	attributes = append(attributes, regular...)
+	return attributes, nil
 }
