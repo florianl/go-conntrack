5c0ffe2c93bc7b2baa2a9ed1dab49400e7145580
Lehner Florian <dev@der-flo.net>
2018-07-21 12:16:39 +0200

    Add BPF
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ bpf.go
@@ -0,0 +1,162 @@
+// +build linux,!386
+
+package conntrack
+
+import (
+	"encoding/binary"
+	"errors"
+
+	"golang.org/x/net/bpf"
+)
+
+// Various errors which may occour when processing filters
+var (
+	ErrFilterLength                  = errors.New("Number of filtering instructions are too high")
+	ErrFilterAttributeLength         = errors.New("Incorrect length of filter attribute")
+	ErrFilterAttributeNotImplemented = errors.New("Filter attribute not implemented")
+)
+
+// various consts from include/uapi/linux/bpf_common.h
+const (
+	// Instruction classes
+	bpfLD   = 0x00 /* copy a value into the accumulator */
+	bpfLDX  = 0x01 /* load a value into the	index register */
+	bpfALU  = 0x04 /* perform operation between the accumulator and index register or constant, and store the result back in the accumulator */
+	bpfJMP  = 0x05 /* jump	instruction */
+	bpfRET  = 0x06 /* return instructions terminate the filter program */
+	bpfMISC = 0x07 /* register transfer	instruction */
+	// ld/ldx fields
+	bpfW   = 0x00 /* 32-bit word size */
+	bpfH   = 0x08 /* 16-bit word size */
+	bpfB   = 0x10 /*  8-bit word size */
+	bpfIMM = 0x00 /* constant addressing */
+	bpfABS = 0x20 /* fixed offset */
+	bpfIND = 0x40 /* variable offset */
+	// alu/jmp fields
+	bpfADD = 0x00
+	bpfAND = 0x50
+	bpfJA  = 0x00
+	bpfJEQ = 0x10
+	bpfK   = 0x00
+
+	// include/uapi/linux/filter.h
+	bpfTAX = 0x00
+	bpfTXA = 0x80
+
+	bpfMAXINSTR = 4096
+
+	bpfVerdictAccept = 0xffffffff
+	bpfVerdictReject = 0x00000000
+)
+
+type filterCheckStruct struct {
+	ct, len int
+}
+
+var filterCheck = map[ConnAttrType]filterCheckStruct{
+	AttrID:     {ct: ctaID, len: 4},
+	AttrUse:    {ct: ctaUse, len: 4},
+	AttrStatus: {ct: ctaStatus, len: 4},
+}
+
+func filterAttribute(filter ConnAttr) []bpf.RawInstruction {
+	var raw []bpf.RawInstruction
+
+	// sizeof(nlmsghdr) + sizeof(nfgenmsg) = 14
+	tmp := bpf.RawInstruction{Op: bpfLD | bpfIMM, K: 14}
+	raw = append(raw, tmp)
+
+	// find attribute type
+	tmp = bpf.RawInstruction{Op: bpfLDX | bpfIMM, K: uint32(filterCheck[filter.Type].ct)}
+	raw = append(raw, tmp)
+	tmp = bpf.RawInstruction{Op: bpfLD | bpfB | bpfABS, K: 0xfffff00c}
+	raw = append(raw, tmp)
+
+	// if A == 0, skip next two
+	tmp = bpf.RawInstruction{Op: bpfJMP | bpfJEQ | bpfK, K: 0, Jt: 2}
+	raw = append(raw, tmp)
+
+	tmp = bpf.RawInstruction{Op: bpfMISC | bpfTAX}
+	raw = append(raw, tmp)
+
+	tmp = bpf.RawInstruction{Op: bpfLD | bpfIND | bpfW, K: 4}
+	raw = append(raw, tmp)
+
+	tmp = bpf.RawInstruction{Op: bpfMISC | bpfTAX}
+	raw = append(raw, tmp)
+
+	val := binary.BigEndian.Uint32(filter.Data)
+	tmp = bpf.RawInstruction{Op: bpfJMP | bpfJEQ | bpfK, K: val, Jt: 2}
+	raw = append(raw, tmp)
+
+	tmp = bpf.RawInstruction{Op: bpfMISC | bpfTXA}
+	raw = append(raw, tmp)
+
+	tmp = bpf.RawInstruction{Op: bpfRET | bpfK, K: 0}
+	raw = append(raw, tmp)
+
+	return raw
+}
+
+// create filter instructions, to check for the subsystem
+func filterSubsys(subsys uint32) []bpf.RawInstruction {
+	var raw []bpf.RawInstruction
+
+	// Offset between start nlmshdr to nlmsg_type in byte
+	tmp := bpf.RawInstruction{Op: bpfLDX | bpfIMM, K: 4}
+	raw = append(raw, tmp)
+
+	// Size of the subsytem id in byte
+	tmp = bpf.RawInstruction{Op: bpfLD | bpfB | bpfIND, K: 1}
+	raw = append(raw, tmp)
+
+	// A == subsys ? jump + 1 : reject
+	tmp = bpf.RawInstruction{Op: bpfJMP | bpfJEQ | bpfK, Jt: 1, K: uint32(subsys)}
+	raw = append(raw, tmp)
+
+	// verdict -> reject
+	tmp = bpf.RawInstruction{Op: bpfRET | bpfK, K: bpfVerdictReject}
+	raw = append(raw, tmp)
+
+	return raw
+}
+
+func constructFilter(subsys CtTable, filters []ConnAttr) ([]bpf.RawInstruction, error) {
+	var raw []bpf.RawInstruction
+
+	tmp := filterSubsys(uint32(subsys))
+	raw = append(raw, tmp...)
+
+	for _, filter := range filters {
+		if _, ok := filterCheck[filter.Type]; !ok {
+			return nil, ErrFilterAttributeNotImplemented
+		}
+		if len(filter.Data) != filterCheck[filter.Type].len {
+			return nil, ErrFilterAttributeLength
+		}
+		tmp = filterAttribute(filter)
+		raw = append(raw, tmp...)
+	}
+
+	// final verdict -> Accept
+	finalVerdict := bpf.RawInstruction{Op: bpfRET | bpfK, K: bpfVerdictAccept}
+	raw = append(raw, finalVerdict)
+
+	if len(raw) >= bpfMAXINSTR {
+		return nil, ErrFilterLength
+	}
+	return raw, nil
+}
+
+func (nfct *Nfct) attachFilter(subsys CtTable, filters []ConnAttr) error {
+
+	bpfFilters, err := constructFilter(subsys, filters)
+	if err != nil {
+		return err
+	}
+	return nfct.con.SetBPF(bpfFilters)
+}
+
+func (nfct *Nfct) removeFilter() error {
+	return nfct.con.RemoveBPF()
+}
