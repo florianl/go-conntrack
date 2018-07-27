c73cc7977b401e57610b592025472e259f5936ab
Lehner Florian <dev@der-flo.net>
2018-07-27 19:46:05 +0200

    Refactor code
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- nest.go
+++ nest.go
@@ -15,85 +15,6 @@ var (
 	ErrAttrNotExist       = errors.New("Type of attribute does not exist")
 )
 
-type attrCheckStruct struct {
-	len, ct int
-}
-
-var attrCheck = map[ConnAttrType]attrCheckStruct{
-	AttrOrigIPv4Src:             {len: 4, ct: ctaIPv4Src},
-	AttrOrigIPv4Dst:             {len: 4, ct: ctaIPv4Dst},
-	AttrReplIPv4Src:             {len: 4, ct: ctaIPv4Src},
-	AttrReplIPv4Dst:             {len: 4, ct: ctaIPv4Dst},
-	AttrOrigIPv6Src:             {len: 16, ct: ctaIPv6Src},
-	AttrOrigIPv6Dst:             {len: 16, ct: ctaIPv6Dst},
-	AttrReplIPv6Src:             {len: 16, ct: ctaIPv6Src},
-	AttrReplIPv6Dst:             {len: 16, ct: ctaIPv6Dst},
-	AttrOrigPortSrc:             {len: 2, ct: ctaUnspec},
-	AttrOrigPortDst:             {len: 2, ct: ctaUnspec},
-	AttrReplPortSrc:             {len: 2, ct: ctaUnspec},
-	AttrReplPortDst:             {len: 2, ct: ctaUnspec},
-	AttrIcmpType:                {len: 1, ct: ctaUnspec},
-	AttrIcmpCode:                {len: 1, ct: ctaUnspec},
-	AttrIcmpID:                  {len: 2, ct: ctaUnspec},
-	AttrOrigL3Proto:             {len: 1, ct: ctaUnspec},
-	AttrReplL3Proto:             {len: 1, ct: ctaUnspec},
-	AttrOrigL4Proto:             {len: 1, ct: ctaProtoNum},
-	AttrReplL4Proto:             {len: 1, ct: ctaProtoNum},
-	AttrTCPState:                {len: 1, ct: ctaUnspec},
-	AttrSNatIPv4:                {len: 4, ct: ctaUnspec},
-	AttrDNatIPv4:                {len: 4, ct: ctaUnspec},
-	AttrSNatPort:                {len: 2, ct: ctaUnspec},
-	AttrDNatPort:                {len: 2, ct: ctaUnspec},
-	AttrTimeout:                 {len: 4, ct: ctaTimeout},
-	AttrMark:                    {len: 4, ct: ctaMark},
-	AttrOrigCounterPackets:      {len: 8, ct: ctaUnspec},
-	AttrReplCounterPackets:      {len: 8, ct: ctaUnspec},
-	AttrOrigCounterBytes:        {len: 8, ct: ctaUnspec},
-	AttrReplCounterBytes:        {len: 8, ct: ctaUnspec},
-	AttrUse:                     {len: 4, ct: ctaUse},
-	AttrID:                      {len: 4, ct: ctaID},
-	AttrStatus:                  {len: 4, ct: ctaStatus},
-	AttrTCPFlagsOrig:            {len: 1, ct: ctaUnspec},
-	AttrTCPFlagsRepl:            {len: 1, ct: ctaUnspec},
-	AttrTCPMaskOrig:             {len: 1, ct: ctaUnspec},
-	AttrTCPMaskRepl:             {len: 1, ct: ctaUnspec},
-	AttrMasterIPv4Src:           {len: 4, ct: ctaUnspec},
-	AttrMasterIPv4Dst:           {len: 4, ct: ctaUnspec},
-	AttrMasterIPv6Src:           {len: 16, ct: ctaUnspec},
-	AttrMasterIPv6Dst:           {len: 16, ct: ctaUnspec},
-	AttrMasterPortSrc:           {len: 2, ct: ctaUnspec},
-	AttrMasterPortDst:           {len: 2, ct: ctaUnspec},
-	AttrMasterL3Proto:           {len: 1, ct: ctaUnspec},
-	AttrMasterL4Proto:           {len: 1, ct: ctaUnspec},
-	AttrSecmark:                 {len: 4, ct: ctaSecmark},
-	AttrOrigNatSeqCorrectionPos: {len: 4, ct: ctaUnspec},
-	AttrOrigNatSeqOffsetBefore:  {len: 4, ct: ctaUnspec},
-	AttrOrigNatSeqOffsetAfter:   {len: 4, ct: ctaUnspec},
-	AttrReplNatSeqCorrectionPos: {len: 4, ct: ctaUnspec},
-	AttrReplNatSeqOffsetBefore:  {len: 4, ct: ctaUnspec},
-	AttrReplNatSeqOffsetAfter:   {len: 4, ct: ctaUnspec},
-	AttrSctpState:               {len: 1, ct: ctaUnspec},
-	AttrSctpVtagOrig:            {len: 4, ct: ctaUnspec},
-	AttrSctpVtagRepl:            {len: 4, ct: ctaUnspec},
-	AttrHelperName:              {len: 30, ct: ctaUnspec},
-	AttrDccpState:               {len: 1, ct: ctaUnspec},
-	AttrDccpRole:                {len: 1, ct: ctaUnspec},
-	AttrDccpHandshakeSeq:        {len: 8, ct: ctaUnspec},
-	AttrTCPWScaleOrig:           {len: 1, ct: ctaUnspec},
-	AttrTCPWScaleRepl:           {len: 1, ct: ctaUnspec},
-	AttrZone:                    {len: 2, ct: ctaZone},
-	AttrSecCtx:                  {len: 30, ct: ctaUnspec},
-	AttrTimestampStart:          {len: 8, ct: ctaUnspec},
-	AttrTimestampStop:           {len: 8, ct: ctaUnspec},
-	AttrHelperInfo:              {len: 30, ct: ctaUnspec},
-	AttrConnlabels:              {len: 30, ct: ctaUnspec},
-	AttrConnlabelsMask:          {len: 30, ct: ctaUnspec},
-	AttrOrigzone:                {len: 2, ct: ctaUnspec},
-	AttrReplzone:                {len: 2, ct: ctaUnspec},
-	AttrSNatIPv6:                {len: 16, ct: ctaUnspec},
-	AttrDNatIPv6:                {len: 16, ct: ctaUnspec},
-}
-
 func nestSubTuple(tupleType uint16, sub []netlink.Attribute) ([]byte, error) {
 	attr, err := netlink.MarshalAttributes(sub)
 	if err != nil {
@@ -107,24 +28,29 @@ func nestSubTuple(tupleType uint16, sub []netlink.Attribute) ([]byte, error) {
 	return tuple.MarshalBinary()
 }
 
-func nestDirTuple(dir int, ipTuple, protoTuple []netlink.Attribute) ([]byte, error) {
-	ipSub, err := nestSubTuple(1, ipTuple)
-	if err != nil {
-		return nil, err
+func nestTuples(attrs []ConnAttr) ([]byte, error) {
+	subTuples := make(map[uint32][]netlink.Attribute)
+	var nestingTuple uint32
+	for _, x := range attrs {
+		if nestingTuple == 0 {
+			nestingTuple = filterCheck[x.Type].nest[0]
+		}
+		subNest := filterCheck[x.Type].nest[1]
+		subTuples[subNest] = append(subTuples[subNest], netlink.Attribute{Type: uint16(filterCheck[x.Type].ct), Data: x.Data})
 	}
-	protoSub, err := nestSubTuple(2, protoTuple)
-	if err != nil {
-		return nil, err
-	}
+
 	var tuple netlink.Attribute
-	if dir == 1 {
-		tuple.Type = ctaTupleOrig | nlafNested
-	} else {
-		tuple.Type = ctaTupleReply | nlafNested
+	tuple.Type = uint16(nestingTuple) | nlafNested
+	var data []byte
+	for i, x := range subTuples {
+		tmp, err := nestSubTuple(uint16(i), x)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, tmp...)
 	}
-	tuple.Length = uint16(len(ipSub) + len(protoSub) + 4)
-	tuple.Data = ipSub
-	tuple.Data = append(tuple.Data, protoSub...)
+	tuple.Length = uint16(len(data) + 4)
+	tuple.Data = data
 
 	return tuple.MarshalBinary()
 }
@@ -132,51 +58,77 @@ func nestDirTuple(dir int, ipTuple, protoTuple []netlink.Attribute) ([]byte, err
 func nestAttributes(filters []ConnAttr) ([]byte, error) {
 	var attributes []byte
 	var attrs []netlink.Attribute
-	var tupleOrig, tupleRepl []netlink.Attribute
-	var protoOrig, protoRepl []netlink.Attribute
+	var tupleOrig, tupleRepl, tupleProto []ConnAttr
 
 	for _, filter := range filters {
-		if _, ok := attrCheck[filter.Type]; !ok {
+		if _, ok := filterCheck[filter.Type]; !ok {
 			return nil, ErrAttrNotExist
 		}
-		if attrCheck[filter.Type].ct == ctaUnspec {
+		if filterCheck[filter.Type].ct == ctaUnspec {
 			return nil, ErrAttrNotImplemented
 		}
-		if len(filter.Data) != attrCheck[filter.Type].len {
+		if len(filter.Data) != filterCheck[filter.Type].len {
 			return nil, ErrAttrLength
 		}
 		if filter.Type == AttrOrigIPv4Src ||
 			filter.Type == AttrOrigIPv4Dst ||
 			filter.Type == AttrOrigIPv6Src ||
-			filter.Type == AttrOrigIPv6Dst {
-			tupleOrig = append(tupleOrig, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+			filter.Type == AttrOrigIPv6Dst ||
+			filter.Type == AttrOrigPortSrc ||
+			filter.Type == AttrOrigPortDst ||
+			filter.Type == AttrIcmpType ||
+			filter.Type == AttrIcmpCode ||
+			filter.Type == AttrIcmpID {
+			tupleOrig = append(tupleOrig, filter)
 		} else if filter.Type == AttrReplIPv4Src ||
 			filter.Type == AttrReplIPv4Dst ||
 			filter.Type == AttrReplIPv6Src ||
-			filter.Type == AttrReplIPv6Dst {
-			tupleRepl = append(tupleRepl, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
-		} else if filter.Type == AttrOrigL4Proto {
-			protoOrig = append(protoOrig, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
-		} else if filter.Type == AttrReplL4Proto {
-			protoRepl = append(protoRepl, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+			filter.Type == AttrReplIPv6Dst ||
+			filter.Type == AttrReplPortSrc ||
+			filter.Type == AttrReplPortDst ||
+			filter.Type == AttrIcmpType ||
+			filter.Type == AttrIcmpCode ||
+			filter.Type == AttrIcmpID {
+			tupleRepl = append(tupleRepl, filter)
+		} else if filter.Type == AttrTCPFlagsOrig ||
+			filter.Type == AttrTCPFlagsRepl ||
+			filter.Type == AttrTCPState ||
+			filter.Type == AttrTCPWScaleOrig ||
+			filter.Type == AttrTCPWScaleRepl ||
+			filter.Type == AttrSctpState ||
+			filter.Type == AttrSctpVtagOrig ||
+			filter.Type == AttrSctpVtagRepl ||
+			filter.Type == AttrDccpState ||
+			filter.Type == AttrDccpRole ||
+			filter.Type == AttrDccpHandshakeSeq {
+			tupleProto = append(tupleProto)
 		} else {
-			attrs = append(attrs, netlink.Attribute{Type: uint16(attrCheck[filter.Type].ct), Data: filter.Data})
+			attrs = append(attrs, netlink.Attribute{Type: uint16(filterCheck[filter.Type].ct), Data: filter.Data})
 		}
 	}
 
-	if len(tupleOrig) != 0 || len(protoOrig) != 0 {
-		data, err := nestDirTuple(1, tupleOrig, protoOrig)
+	if len(tupleOrig) != 0 {
+		data, err := nestTuples(tupleOrig)
+		if err != nil {
+			return nil, err
+		}
+		hexData(data)
+		attributes = append(attributes, data...)
+	}
+	if len(tupleRepl) != 0 {
+		data, err := nestTuples(tupleRepl)
 		if err != nil {
 			return nil, err
 		}
 		attributes = append(attributes, data...)
 	}
-	if len(tupleRepl) != 0 || len(protoRepl) != 0 {
-		data, err := nestDirTuple(0, tupleRepl, protoRepl)
+	if len(tupleProto) != 0 {
+		data, err := nestTuples(tupleProto)
 		if err != nil {
 			return nil, err
 		}
 		attributes = append(attributes, data...)
+
 	}
 
 	regular, err := netlink.MarshalAttributes(attrs)
