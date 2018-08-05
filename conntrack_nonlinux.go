bdd8ea2bc1d05064e8edd88de9c232ab0de0afbd
Lehner Florian <dev@der-flo.net>
2018-08-05 07:05:07 +0200

    Fix non-linux tests
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- conntrack_nonlinux.go
+++ conntrack_nonlinux.go
@@ -19,15 +19,29 @@ const (
 var ErrNotLinux = errors.New("package requires the netfilter subsystem of the linux kernel")
 
 // Open returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
-func Open() (*Nfct, error)                                                   { return nil, ErrNotLinux }
-func (nfct *Nfct) Close() error                                              { return ErrNotLinux }
-func (nfct *Nfct) Dump(_ CtTable, _ CtFamily) ([]Conn, error)                { return nil, ErrNotLinux }
-func (nfct *Nfct) Flush(_ CtTable, _ CtFamily) error                         { return ErrNotLinux }
-func (nfct *Nfct) Create(_ CtTable, _ CtFamily, _ []ConnAttr) error          { return ErrNotLinux }
+func Open() (*Nfct, error) { return nil, ErrNotLinux }
+
+// Close returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
+func (nfct *Nfct) Close() error { return ErrNotLinux }
+
+// Dump returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
+func (nfct *Nfct) Dump(_ CtTable, _ CtFamily) ([]Conn, error) { return nil, ErrNotLinux }
+
+// Flush returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
+func (nfct *Nfct) Flush(_ CtTable, _ CtFamily) error { return ErrNotLinux }
+
+// Create returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
+func (nfct *Nfct) Create(_ CtTable, _ CtFamily, _ []ConnAttr) error { return ErrNotLinux }
+
+// Query returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
 func (nfct *Nfct) Query(_ CtTable, _ CtFamily, _ FilterAttr) ([]Conn, error) { return nil, ErrNotLinux }
+
+// Register returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
 func (nfct *Nfct) Register(_ context.Context, _ CtTable, _ NetlinkGroup, _ func(c Conn) int) (<-chan error, error) {
 	return nil, ErrNotLinux
 }
+
+// RegisterFiltered returns an error, as this packages highly depends on the netfilter subsystem of the linux kernel
 func (nfct *Nfct) RegisterFiltered(_ context.Context, _ CtTable, _ NetlinkGroup, _ []ConnAttr, _ func(c Conn) int) (<-chan error, error) {
 	return nil, ErrNotLinux
 }
