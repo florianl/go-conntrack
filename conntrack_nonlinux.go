921ba0538c51e59d13b2a4be2dc467156df30a91
Lehner Florian <dev@der-flo.net>
2018-12-19 21:46:01 +0100

    Add another non-linux stub
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- conntrack_nonlinux.go
+++ conntrack_nonlinux.go
@@ -51,3 +51,8 @@ func ParseAttributes(_ []byte) (Conn, error) { return nil, ErrNotLinux }
 
 // Update an existing conntrack entry
 func (nfct *Nfct) Update(t CtTable, f CtFamily, attributes []ConnAttr) error { return ErrNotLinux }
+
+// Get returns matching conntrack entries with certain attributes
+func (nfct *Nfct) Get(_ CtTable, _ CtFamily, _ []ConnAttr) ([]Conn, error) {
+	return nil, ErrNotLinux
+}
