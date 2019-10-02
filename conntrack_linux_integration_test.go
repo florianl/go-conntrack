50faf567f77735b44fcbf54384ac7986b11927b2
Florian Lehner <florianl@users.noreply.github.com>
2019-10-02 12:14:13 +0200

    Update integration test (#16)
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- conntrack_linux_integration_test.go
+++ conntrack_linux_integration_test.go
@@ -4,43 +4,24 @@ package conntrack
 
 import (
 	"context"
+	"net"
 	"os/exec"
 	"testing"
 	"time"
 )
 
-/*
- * returns 0, if both contain the same values
- * returns -1, if size differs
- * returns >0, the position, on which the values are different
- */
-func compare(a, b []byte) int {
-	var diff = 1
-	if len(a) != len(b) {
-		return -1
-	}
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
-			return diff
-		}
-		diff++
-	}
-	return 0
-}
-
 func TestLinuxConntrackUpdatePing(t *testing.T) {
-	t.Skip()
 	// ping is needed to create a session, we can work with
 	_, err := exec.LookPath("ping")
 	if err != nil {
-		t.Fatalf("Could not find ping binary")
+		t.Fatalf("could not find ping binary")
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	// Create a session
 	cmd := exec.CommandContext(ctx, "ping", "127.0.0.2")
 	if err := cmd.Start(); err != nil {
-		t.Fatalf("Could not start ping to 127.0.0.2: %v", err)
+		t.Fatalf("could not start ping to 127.0.0.2: %v", err)
 	}
 	defer cancel()
 
@@ -49,36 +30,51 @@ func TestLinuxConntrackUpdatePing(t *testing.T) {
 
 	nfct, err := Open(&Config{})
 	if err != nil {
-		t.Fatalf("Could not open socket: %v", err)
+		t.Fatalf("could not open socket: %v", err)
 	}
 	defer nfct.Close()
 
-	conns, err := nfct.Dump(Conntrack, IPv4)
+	cons, err := nfct.Dump(Conntrack, IPv4)
 	if err != nil {
-		t.Fatalf("Could not dump sessions: %v", err)
+		t.Fatalf("could not dump sessions: %v", err)
 	}
 
-	for _, c := range conns {
-
+	var pingSession Con
+	for _, c := range cons {
+		if *c.Origin.Proto.Number != 1 {
+			continue
+		}
+		if (*c.Origin.Src).Equal(net.ParseIP("127.0.0.1")) && (*c.Origin.Dst).Equal(net.ParseIP("127.0.0.2")) {
+			pingSession = c
+			break
+		}
 	}
+
+	origMark := *pingSession.Mark
+
+	*pingSession.Mark = 0xFF00AA11
 
 	// Update the conntrack entry
-	if err := nfct.Update(Conntrack, IPv4, attrs); err != nil {
-		t.Fatalf("Could not update conntrack entry: %v", err)
+	if err := nfct.Update(Conntrack, IPv4, pingSession); err != nil {
+		t.Fatalf("could not update conntrack entry: %v", err)
 	}
 
-	c, err := nfct.Get(Conntrack, IPv4, attrs)
+	c, err := nfct.Get(Conntrack, IPv4, pingSession)
 	if err != nil {
-		t.Fatalf("Could not get session: %v", err)
-	}
-	if len(c) != 1 {
-		t.Fatalf("Could not find unique ping sessiond")
+		t.Fatalf("could not get session: %v", err)
 	}
 
+	if len(c) != 1 {
+		t.Fatalf("could not get updated session")
+	}
+
+	if origMark == *c[0].Mark {
+		t.Fatalf("could not update mark of the session")
+	}
+	t.Logf("original mark 0x%x vs modified mark 0x%x\n", origMark, *c[0].Mark)
 }
 
 func TestLinuxConntrackDeleteEntry(t *testing.T) {
-	t.Skip()
 	// ping is needed to create a session, we can work with
 	_, err := exec.LookPath("ping")
 	if err != nil {
@@ -102,26 +98,21 @@ func TestLinuxConntrackDeleteEntry(t *testing.T) {
 	}
 	defer nfct.Close()
 
-	conns, err := nfct.Dump(Ct, CtIPv4)
+	conns, err := nfct.Dump(Conntrack, IPv4)
 	if err != nil {
 		t.Fatalf("Could not dump sessions: %v", err)
 	}
 
-	origSessID := []byte{}
+	var origConntrackID uint32
 
 	for _, c := range conns {
-		if compare(c[AttrOrigIPv4Dst], []byte{127, 0, 0, 4}) == 0 {
-			sess := []ConnAttr{
-				{Type: AttrOrigIPv4Dst, Data: c[AttrOrigIPv4Dst]},
-				{Type: AttrOrigIPv4Src, Data: c[AttrOrigIPv4Src]},
-				{Type: AttrOrigL4Proto, Data: c[AttrOrigL4Proto]},
-				{Type: AttrIcmpType, Data: []byte{8}},
-				{Type: AttrIcmpCode, Data: []byte{0}},
-				{Type: AttrIcmpID, Data: c[AttrIcmpID]},
-			}
-			origSessID = c[AttrID]
-			if err := nfct.Delete(Ct, CtIPv4, sess); err != nil {
-				t.Fatalf("Could not delete session: %v", err)
+		if *c.Origin.Proto.Number != 1 {
+			continue
+		}
+		if (*c.Origin.Src).Equal(net.ParseIP("127.0.0.1")) && (*c.Origin.Dst).Equal(net.ParseIP("127.0.0.4")) {
+			origConntrackID = *c.ID
+			if err := nfct.Delete(Conntrack, IPv4, c); err != nil {
+				t.Fatalf("could not delete session: %v", err)
 			}
 			break
 		}
@@ -129,15 +120,15 @@ func TestLinuxConntrackDeleteEntry(t *testing.T) {
 
 	// there will be a session for the ping, as it is still running.
 	// But as we deleted the original session, there has to be a new AttrID
-	conns2, err2 := nfct.Dump(Ct, CtIPv4)
+	conns2, err2 := nfct.Dump(Conntrack, IPv4)
 	if err2 != nil {
-		t.Fatalf("Could not dump sessions: %v", err)
+		t.Fatalf("could not dump sessions: %v", err)
 	}
 
 	for _, c := range conns2 {
-		if compare(c[AttrOrigIPv4Dst], []byte{127, 0, 0, 4}) == 0 {
-			if compare(c[AttrID], origSessID) == 0 {
-				t.Fatalf("Original ping session was not deleted")
+		if (*c.Origin.Src).Equal(net.ParseIP("127.0.0.1")) && (*c.Origin.Dst).Equal(net.ParseIP("127.0.0.4")) {
+			if *c.ID == origConntrackID {
+				t.Fatalf("original ping session was not deleted")
 			}
 		}
 	}
