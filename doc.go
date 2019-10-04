c8dd721a1d662a7256890e651b84d2c196b307f9
Lehner Florian <dev@der-flo.net>
2019-10-04 20:33:22 +0200

    update doc
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- doc.go
+++ doc.go
@@ -3,32 +3,32 @@ Package conntrack provides an API to interact with the conntrack subsystem of th
 
 Example:
 
-package main
-import (
-	"fmt"
-
-	ct "github.com/florianl/go-conntrack"
-)
-
-func main(){
-	nfct, err := ct.Open(&ct.Config{})
-	if err != nil {
-		fmt.Println("Could not create nfct:", err)
-		return
+	package main
+	import (
+		"fmt"
+		ct "github.com/florianl/go-conntrack"
+	)
+	func main(){
+		nfct, err := ct.Open(&ct.Config{})
+		if err != nil {
+			fmt.Println("Could not create nfct:", err)
+			return
+		}
+		defer nfct.Close()
+		sessions, err := nfct.Dump(ct.Conntrack, ct.IPv4)
+		if err != nil {
+			fmt.Println("Could not dump sessions:", err)
+			return
+		}
+		for _, session := range sessions {
+			fmt.Printf("[%2d] %s - %s\n", session.Origin.Proto.Number, session.Origin.Src, session.Origin.Dst)
+		}
 	}
-	defer nfct.Close()
 
-	sessions, err := nfct.Dump(ct.Conntrack, ct.IPv4)
-	if err != nil {
-		fmt.Println("Could not dump sessions:", err)
-		return
-	}
+This package processes information directly from the kernel and therefore it requires special privileges. You
+can provide this privileges by adjusting the CAP_NET_ADMIN capabilities.
 
-	for _, session := range sessions {
-		fmt.Printf("[%2d] %s - %s\n", session.Origin.Proto.Number, session.Origin.Src, session.Origin.Dst)
-	}
-}
-
+	setcap 'cap_net_admin=+ep' /your/executable
 
 */
 package conntrack
