84ddc8ca7cc094ab02c7958c05698c78d24f327d
Lehner Florian <dev@der-flo.net>
2019-11-12 11:47:09 +0100

    add expect attributes
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ attributeExpect.go
@@ -0,0 +1,135 @@
+package conntrack
+
+import (
+	"encoding/binary"
+	"log"
+
+	"github.com/mdlayher/netlink"
+)
+
+const (
+	ctaExpUnspec = iota
+	ctaExpMaster
+	ctaExpTuple
+	ctaExpMask
+	ctaExpTimeout
+	ctaExpID
+	ctaExpHelpName
+	ctaExpZone
+	ctaExpFlags
+	ctaExpClass
+	ctaExpNat
+	ctaExpFn
+)
+
+const (
+	ctaExpNatUnspec = iota
+	ctaExpNatDir
+	ctaExpNatTuple
+)
+
+func extractNatInfo(v *NatInfo, logger *log.Logger, data []byte) error {
+	ad, err := netlink.NewAttributeDecoder(data)
+	if err != nil {
+		return err
+	}
+	ad.ByteOrder = nativeEndian
+	for ad.Next() {
+		switch ad.Type() {
+		case ctaExpNatDir:
+			ad.ByteOrder = binary.BigEndian
+			tmp := ad.Uint32()
+			v.Dir = &tmp
+			ad.ByteOrder = nativeEndian
+		case ctaExpNatTuple:
+			tuple := &IPTuple{}
+			if err := extractIPTuple(tuple, logger, ad.Bytes()); err != nil {
+				return err
+			}
+			v.Tuple = tuple
+		default:
+			logger.Printf("extractNatInfo(): %d | %d\t %v", ad.Type(), ad.Type()&0xFF, ad.Bytes())
+		}
+	}
+	return ad.Err()
+}
+
+func extractAttributeExpect(c *Con, logger *log.Logger, data []byte) error {
+	ad, err := netlink.NewAttributeDecoder(data)
+	if err != nil {
+		return err
+	}
+	for ad.Next() {
+		switch ad.Type() {
+		case ctaExpMaster:
+			tuple := &IPTuple{}
+			if err := extractIPTuple(tuple, logger, ad.Bytes()); err != nil {
+				return err
+			}
+			c.Origin = tuple
+		case ctaExpTuple:
+			tuple := &IPTuple{}
+			if err := extractIPTuple(tuple, logger, ad.Bytes()); err != nil {
+				return err
+			}
+			c.Exp.Tuple = tuple
+		case ctaExpMask:
+			tuple := &IPTuple{}
+			if err := extractIPTuple(tuple, logger, ad.Bytes()); err != nil {
+				return err
+			}
+			c.Exp.Mask = tuple
+		case ctaExpTimeout:
+			ad.ByteOrder = binary.BigEndian
+			tmp := ad.Uint32()
+			c.Exp.Timeout = &tmp
+			ad.ByteOrder = nativeEndian
+		case ctaExpID:
+			ad.ByteOrder = binary.BigEndian
+			tmp := ad.Uint32()
+			c.Exp.ID = &tmp
+			ad.ByteOrder = nativeEndian
+		case ctaExpHelpName:
+			tmp := ad.String()
+			c.Exp.HelperName = &tmp
+		case ctaExpZone:
+			ad.ByteOrder = binary.BigEndian
+			zone := ad.Uint16()
+			c.Exp.Zone = &zone
+			ad.ByteOrder = nativeEndian
+		case ctaExpFlags:
+			ad.ByteOrder = binary.BigEndian
+			tmp := ad.Uint32()
+			c.Exp.Flags = &tmp
+			ad.ByteOrder = nativeEndian
+		case ctaExpClass:
+			ad.ByteOrder = binary.BigEndian
+			tmp := ad.Uint32()
+			c.Exp.Class = &tmp
+			ad.ByteOrder = nativeEndian
+		case ctaExpNat:
+			tmp := &NatInfo{}
+			if err := extractNatInfo(tmp, logger, ad.Bytes()); err != nil {
+				return err
+			}
+			c.Exp.Nat = tmp
+		case ctaExpFn:
+			tmp := ad.String()
+			c.Exp.Fn = &tmp
+		default:
+			logger.Printf("extractAttributeExpect() - Unknown attribute: %d %d %v\n", ad.Type()&0xFF, ad.Type(), ad.Bytes())
+		}
+	}
+	return ad.Err()
+}
+
+func extractExpectAttributes(logger *log.Logger, msg []byte) (Con, error) {
+	c := Con{}
+
+	offset := checkHeader(msg[:2])
+	if err := extractAttributeExpect(&c, logger, msg[offset:]); err != nil {
+		return c, err
+	}
+
+	return c, nil
+}
