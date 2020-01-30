dd1a4235e7ead157862b855e17c68f9aedd56d27
Lehner Florian <dev@der-flo.net>
2020-01-30 20:08:20 +0100

    add expected nesting
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- nest.go
+++ nest.go
@@ -59,6 +59,65 @@ func nestAttributes(logger *log.Logger, filters *Con) ([]byte, error) {
 		}
 		ae.Bytes(ctaProtoinfo|nlafNested, data)
 	}
+	if filters.Exp != nil {
+		if err := nestExpectedAttributes(logger, ae, filters.Exp); err != nil {
+			return []byte{}, err
+		}
+	}
 
 	return ae.Encode()
+}
+
+func nestExpectedAttributes(logger *log.Logger, ae *netlink.AttributeEncoder, filters *Exp) error {
+
+	if filters.Mask != nil {
+		data, err := marshalIPTuple(logger, filters.Mask)
+		if err != nil {
+			return err
+		}
+		ae.Bytes(ctaExpMask|nlafNested, data)
+	}
+	if filters.Tuple != nil {
+		data, err := marshalIPTuple(logger, filters.Tuple)
+		if err != nil {
+			return err
+		}
+		ae.Bytes(ctaExpTuple|nlafNested, data)
+	}
+	if filters.Flags != nil {
+		ae.ByteOrder = binary.BigEndian
+		ae.Uint32(ctaExpFlags, *filters.Flags)
+		ae.ByteOrder = nativeEndian
+	}
+	if filters.Class != nil {
+		ae.ByteOrder = binary.BigEndian
+		ae.Uint32(ctaExpClass, *filters.Class)
+		ae.ByteOrder = nativeEndian
+	}
+	if filters.ID != nil {
+		ae.ByteOrder = binary.BigEndian
+		ae.Uint32(ctaExpID, *filters.ID)
+		ae.ByteOrder = nativeEndian
+	}
+	if filters.Timeout != nil {
+		ae.ByteOrder = binary.BigEndian
+		ae.Uint32(ctaExpTimeout, *filters.Timeout)
+		ae.ByteOrder = nativeEndian
+	}
+	if filters.Zone != nil {
+		ae.ByteOrder = binary.BigEndian
+		ae.Uint16(ctaExpZone, *filters.Zone)
+		ae.ByteOrder = nativeEndian
+	}
+	if filters.HelperName != nil {
+		ae.ByteOrder = binary.BigEndian
+		ae.String(ctaExpHelpName, *filters.HelperName)
+		ae.ByteOrder = nativeEndian
+	}
+	if filters.Fn != nil {
+		ae.ByteOrder = binary.BigEndian
+		ae.String(ctaExpFn, *filters.Fn)
+		ae.ByteOrder = nativeEndian
+	}
+	return nil
 }
