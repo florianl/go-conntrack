59770b29420b3cc98d42b5628f3954504193f50d
Lehner Florian <dev@der-flo.net>
2020-02-14 16:03:04 +0100

    add pkg comment
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- internal/unix/types_linux.go
+++ internal/unix/types_linux.go
@@ -1,5 +1,9 @@
 // +build linux
 
+/*
+	Package unix maps constants from golang.org/x/sys/unix to local constants and makes
+	them available for other platforms as well.
+*/
 package unix
 
 import (
